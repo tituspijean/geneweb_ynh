d94e424f8b7d91ab905793560d8105d9edabf30b
tituspijean <tituspijean@outlook.com>
2019-11-27 01:03:25 +0100

    Initial commit

---
+++ scripts/_common.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+# Detect the system architecture to download the right tarball
+# NOTE: `uname -m` is more accurate and universal than `arch`
+# See https://en.wikipedia.org/wiki/Uname
+if [ -n "$(uname -m | grep 64)" ]; then
+	architecture="amd64"
+elif [ -n "$(uname -m | grep 86)" ]; then
+	architecture="32bit"
+elif [ -n "$(uname -m | grep arm)" ]; then
+	architecture="arm"
+  ynh_die "No binaries are available on arm"
+else
+	ynh_die "Unable to detect your achitecture, please open a bug describing \
+        your hardware and the result of the command \"uname -m\"." 1
+fi
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
